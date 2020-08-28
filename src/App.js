03a33b6d0258b4c2e21082cc7a9711efa930be5d
robert-upwork <robyop.eyeco@gmail.com>
2020-08-29 03:29:38 +0700

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-// import "./App.css";
 import "./App.scss";
 
 import HomePage from "./views/HomePage";
